5397e2627f7e36a159429cc926912ba5da54cf12
Júlio César de Amorim <98892057+julioamorimbr@users.noreply.github.com>
2022-12-23 01:34:40 -0300

    Adicionando o código e o readme

---
+++ README.md
@@ -0,0 +1,9 @@
+Esse projeto deve ser utilizado apenas para fins de estudos e não pode ser utilizado para fins comerciais ou para algo que prejudique terceiros.
+
+Script desenvolvido por Júlio César de Amorim
+
+Utilização:
+
+Preencher formulários automaticamente atrávés de dados anexados em planilha.
+
+Emissões de notas automatizadas e cadastros de usuários.
---
+++ botsendform.py
@@ -0,0 +1,28 @@
+for i, nome in enumerate(tabela["Nome"]):
+    email = tabela.loc[i, "Email"]
+    assunto = tabela.loc[i, "Assunto"]
+    mensagem = tabela.loc[i, "Mensagem"]
+    
+    from selenium import webdriver
+
+    navegador = webdriver.Chrome()
+    navegador.get("LINK AQUI")
+    
+    
+    # preencher Nome
+    navegador.find_element("xpath", '//*[@id="jform_contact_name"]').send_keys(nome)
+
+    # preencher email
+    navegador.find_element("xpath", '//*[@id="jform_contact_email"]').send_keys(email)
+    #preencher assunto
+    navegador.find_element("xpath", '//*[@id="jform_contact_emailmsg"]').send_keys(assunto)
+    #preencher mensagem
+    navegador.find_element("xpath", '//*[@id="jform_contact_message"]').send_keys(mensagem)
+
+    #marcar checkbox
+    marcar = navegador.find_element("xpath", '//*[@id="jform_consentbox0"]')
+    navegador.execute_script("arguments[0].click();", marcar)
+
+    #clicar no botao
+    enviar = navegador.find_element("xpath", '//*[@id="contact-form"]/div/div/button')
+    navegador.execute_script("arguments[0].click();", enviar)
